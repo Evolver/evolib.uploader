579a2c44ab02c3a8b679dc521bb898a08f4c8b46
Dmitry Stepanov <dmitrij@stepanov.lv>
2010-06-20 18:02:06 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+<h1>Extension to Evolver's JavaScript library - uploader</h1>
+
+More info to come...
---
+++ build/build.php
@@ -0,0 +1,35 @@
+<?php
+// Author: Dmitry Stepanov <dmitrij@stepanov.lv>
+// URL: http://www.stepanov.lv
+// Sun Dec 20 21:00:07 GMT 2009 21:00:07 build.php
+
+// where to take source files from
+define( 'SOURCE_ROOT', dirname( __DIR__) .'/');
+
+// compile library file
+function lib_file( $path) {
+  return file_get_contents( SOURCE_ROOT .'lib/' .$path);
+}
+
+// Library package builder
+
+$authorNote =<<<NOTE
+/**
+ * Evolib Uploader - extension to Evolver's JavaScript library.
+ * http://github.com/Evolver/evolib.uploader
+ *
+ * Copyright (C) 2010 Dmitry Stepanov <dmitrij@stepanov.lv>
+ * URL: http://www.stepanov.lv
+ *
+ * Publicly available for non-commercial use under GPL v2 license terms.
+ */
+
+NOTE;
+
+file_put_contents( __DIR__ .'/evolib.controls.js',
+	$authorNote .
+  lib_file( 'uploader.Uploader.js') ."\n\n" .
+  lib_file( 'uploader.js')
+);
+
+?>
